0b4609d041f0b8521460421ba7890434dccdb3ab
Tu276 <tu276nathan@gmail.com>
2022-09-15 14:10:47 +0300

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - main
+  * prints lowercase alphabet followed by a newline
+  * Return: 0
+  */
+
+int main() {
+    // Write C code here
+    for (char c = 'a'; c <= 'z'; ++c)
+
+    putchar (c);
+    putchar ('\n')
+
+    return 0;
+}
