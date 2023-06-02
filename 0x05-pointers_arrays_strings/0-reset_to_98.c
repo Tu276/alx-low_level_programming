8431f82a78699400cd7d1a0e23579d3d892f38b1
Tu276 <tu276nathan@gmail.com>
2023-06-02 11:37:32 +0300

    syntax error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-  * reset_to_98 - takes a pointer to an  int as a parameter and points to 98
-  * @n: pointer to an  int
-  * return: 0
+  * reset_to_98 - takes a pointer to int
+  * @n: pointer to an int
   */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
