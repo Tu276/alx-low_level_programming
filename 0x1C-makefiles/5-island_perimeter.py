3c71b77f2cc82f9c1c8703d3000bee76253968e3
Tu276 <tu276nathan@gmail.com>
2023-11-01 18:01:55 +0300

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
-"""This module returns the perimeter of the island described by grid"""
+"""
+Module for island_perimeter function
+"""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid."""
-
-    perimeter = 0
-
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j]:
-                perimeter += get_surrounding_water(grid, i, j)
-
-    return perimeter
-
-
-def get_surrounding_water(grid, i, j):
     """
-    Returns the number of sides surrounded by water.
+    Returns the perimeter of the island described in grid.
 
-    Args:
-    - grid: a list of lists of integers representing a grid
-    - i: an integer representing the row index
-    - j: an integer representing the column index
+    Parameters:
+    - grid: list of list of integers where 0 represents a water zone and 1 represents a land zone.
 
     Returns:
-    - an integer representing the number of sides surrounded by water
+    - perimeter: integer representing the perimeter of the island.
     """
-    water_sides = 0
-
-    if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-        water_sides += 1
-    if i == 0 or grid[i - 1][j] == 0:
-        water_sides += 1
-    if j == 0 or grid[i][j - 1] == 0:
-        water_sides += 1
-    if i == len(grid) - 1 or grid[i + 1][j] == 0:
-        water_sides += 1
-
-    return water_sides
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+    return perimeter
